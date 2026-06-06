bc7a3be5ef6ad5c393781de6b0145225cfdeb54c
vishwascr <vishwasreddy111@gmail.com>
2026-06-06 15:22:53 +0530

    Backend unreachable fix

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -13,7 +13,7 @@ export default defineConfig({
     host: '0.0.0.0',
     proxy: {
       '/api': {
-        target: 'http://127.0.0.1:3005',
+        target: 'http://localhost:5035',
         changeOrigin: true,
       },
     },
