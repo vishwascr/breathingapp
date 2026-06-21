995b1322af0b07a24b09c686eade8f96690dee5c
vishwascr <vishwasreddy111@gmail.com>
2026-06-21 20:46:10 +0530

    Improved card spacing in dashboard

--- frontend/src/components/common/Card.jsx
+++ frontend/src/components/common/Card.jsx
@@ -45,8 +45,8 @@ const Card = forwardRef(
       none: 'p-0',
       sm: 'p-4 md:p-5',
       md: 'p-6 md:p-8',
-      lg: 'p-8 md:p-10',
-      xl: 'p-6 md:p-12',
+      lg: 'p-10 md:p-12',
+      xl: 'p-12 md:p-16',
     };
 
     // Rounded classes
